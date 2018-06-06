1aee27427a05a605f486ba32b026eccdeb2fe98d
Mehdi <root@email.fr>
2018-06-06 17:14:38 +0200

    add parceur

--- init.py
+++ init.py
@@ -5,6 +5,10 @@ url = "https://pokemondb.net/pokedex/all"
 
 response = requests.get(url)
 html = str(response.content)
-soup = BeautifulSoup(html, 'html.parser')
+soup = BeautifulSoup(html, "html.parser")
 
 tab = soup.find(id="pokedex")
+for link in tab.find_all("tr"):
+    for l in link.find_all("td"):
+        print(l.text)
+        print("\n")
