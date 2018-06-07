49849a1d2950c7798310301bc525e27f157439a7
Mehdi <mehdi@mehdi-aberkane.fr>
2018-06-07 17:51:43 +0200

    add init bdd

--- init.py
+++ init.py
@@ -1,14 +1,31 @@
-import requests, sqlite3
+import requests, mysql.connector
 from bs4 import BeautifulSoup
 
 url = "https://pokemondb.net/pokedex/all"
 
+cnx = mysql.connector.connect(user='root', password='root', host='127.0.0.1')
+
+cursor = cnx.cursor()
+
+cursor.execute('CREATE DATABASE IF NOT EXISTS pokemon')
+cursor.connect('pokemon')
+cursor.execute('CREATE TABLE IF NOT EXISTS pokemon ('
+                'id INT PRIMARY KEY NOT NULL AUTO_INCREMENT',
+                'name varchar(100)',
+               ')')
+cursor.execute('INSERT INTO "pokemon" (name) VALUES ("toto")')
+
+'''
 response = requests.get(url)
 html = str(response.content)
 soup = BeautifulSoup(html, "html.parser")
 
 tab = soup.find(id="pokedex")
 for link in tab.find_all("tr"):
+    tab = []
     for l in link.find_all("td"):
-        print(l.text)
+        tab.append(l.text)
+    if len(tab) > 0:
         print("\n")
+        print(tab)
+'''
